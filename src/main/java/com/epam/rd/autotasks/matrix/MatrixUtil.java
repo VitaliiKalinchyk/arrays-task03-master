c3b01adfa9e428faad6356dcbfe1da1a0bbe16b1
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-05 19:42:35 +0200

    5 11

--- src/main/java/com/epam/rd/autotasks/matrix/MatrixUtil.java
+++ src/main/java/com/epam/rd/autotasks/matrix/MatrixUtil.java
@@ -5,7 +5,29 @@ import java.util.Arrays;
 public class MatrixUtil {
 
 	public static void transformMatrix(int[][] matrix) {
-		// TODO: Implement this method.
+		if (correctMatrix(matrix)) {
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix.length; j++) {
+					if (i > j) {
+						matrix[i][j] = 0;
+					} else if (j > i) {
+						matrix[i][j] = 1;
+					}
+				}
+			}
+		}
+	}
+
+	private static boolean correctMatrix(int[][] matrix) {
+		if (matrix == null || matrix.length == 0) {
+			return false;
+		}
+		for (int[] ints : matrix) {
+			if (ints == null || ints.length == 0 || ints.length != matrix.length) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
